5b1179ff4e6e1e563a2442661e17b4ebd6a91660
Anya <use.anya@gmail.com>
2023-10-20 15:20:08 +0300

    add createAction

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { createAction } from "@reduxjs/toolkit";
+
 export const fetchHeroes = (request, element) => (dispatch) => {
     dispatch(heroesFetching());
     request(`http://localhost:3001/heroes${element}`)
@@ -5,47 +7,17 @@ export const fetchHeroes = (request, element) => (dispatch) => {
         .catch(() => dispatch(heroesFetchingError()));
 }
 
-export const heroesFetching = () => {
-    return {
-        type: 'HEROES_FETCHING'
-    }
-}
+export const heroesFetching = createAction('HEROES_FETCHING');
+export const heroesFetched = createAction('HEROES_FETCHED');
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
 
-export const heroesFetched = (heroes) => {
-    return {
-        type: 'HEROES_FETCHED',
-        payload: heroes
-    }
-}
-
-export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
-
-export const fetchFilters = (request) => (dispatch) =>{
+export const fetchFilters = (request) => (dispatch) => {
     dispatch(filtersFetching);
     request("http://localhost:3001/filters")
         .then((data) => dispatch(filtersFetched(data)))
         .catch(() => dispatch(filtersFetchingError()));
 }
 
-export const filtersFetching = () => {
-    return {
-        type: 'FILTERS_FETCHING'
-    }
-}
-
-export const filtersFetched = (filters) => {
-    return {
-        type: 'FILTERS_FETCHED',
-        payload: filters
-    }
-}
-
-export const filtersFetchingError = () => {
-    return {
-        type: 'FILTERS_FETCHING_ERROR'
-    }
-}
+export const filtersFetching = createAction('FILTERS_FETCHING');
+export const filtersFetched = createAction('FILTERS_FETCHED');
+export const filtersFetchingError = createAction('FILTERS_FETCHING_ERROR');
